b953b883e0c2787e4efeef03544338efea5909ec
Tinjo <jelte.friedrich2018@gmail.com>
2025-04-01 03:19:43 +0200

    adding worker js for cloudflare

--- script/index.js
+++ script/index.js
@@ -1,3 +1,12 @@
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  async function handleRequest(request) {
+    return new Response('Hello from Cloudflare Workers!', { status: 200 })
+  }  
+
 'use strict';
 
 // Erstellen der Dom-Video-Elemente
