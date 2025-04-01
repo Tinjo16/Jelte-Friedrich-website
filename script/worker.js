ab39e1f7c1ae77bef621f7310185ad25a9dfdac8
Tinjo <jelte.friedrich2018@gmail.com>
2025-04-01 13:09:21 +0200

    split js code into worker.js

---
+++ script/worker.js
@@ -0,0 +1,31 @@
+// password protected directory
+
+addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request));
+  });
+  
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+  
+    // Falls das Verzeichnis "/protected" ist
+    if (url.pathname.startsWith("https://tinjo16.github.io/jelte-friedrich-website/Pages/family/main.html")) {
+      const auth = request.headers.get("Authorization");
+  
+      // Nutzername und Passwort (einfacher Schutz)
+      const username = "admin";
+      const password = "admin";
+      const encoded = btoa(`${username}:${password}`);
+  
+      if (!auth || auth !== `Basic ${encoded}`) { 
+        return new Response("Unauthorized", {
+          status: 401,
+          headers: {
+            "WWW-Authenticate": 'Basic realm="Secure Area"'
+          }
+        });
+      }
+    }
+  
+    return fetch(request);
+  }
+  
